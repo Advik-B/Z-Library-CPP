1f0a7c938136a20c5ae463a8006bf7bd4f2aa861
Advik <advik.b@gmail.com>
2025-04-06 20:32:18 +0530

    Update API.hpp

--- include/zlibrary/API.hpp
+++ include/zlibrary/API.hpp
@@ -3,15 +3,10 @@
 #define ZLIBRARYAPI_HPP
 
 #include <string>
-#include <vector>
-#include <optional>
 #include <functional>
 #include <filesystem>
 #include "Export.hpp"
 #include "Types.hpp"     // Include the new types header
-#include "Enums.hpp"     // Include the new enums header
-#include "BookSearchResult.hpp"
-#include "Book.hpp"
 #include <cpr/cpr.h>
 
 namespace fs = std::filesystem;
@@ -23,7 +18,6 @@ private:
     LogCallback log_callback_;
 
     void setup_session_defaults();
-    BookInfo fromHTML(const std::string& htmlContentUtf8);
     void log(LogLevel level, const std::string& module, const std::string& msg);
 
 public:
@@ -37,22 +31,6 @@ public:
     void setLogCallback(LogCallback logger);
     void setBaseUrl(const std::string& base_url);
     [[nodiscard]] const std::string& getBaseUrl() const;
-
-    // --- Updated Public API Methods ---
-    std::vector<BookSearchResult> searchBooks(
-        const std::string& query,
-        const std::optional<zlib::OrderOptions>& order = std::nullopt, // Use enum type
-        bool exact_match = false,
-        const std::optional<int>& year_from = std::nullopt,
-        const std::optional<int>& year_to = std::nullopt,
-        const std::vector<zlib::Language>& languages = {},    // Use enum type
-        const std::vector<zlib::Extension>& extensions = {} // Use enum type
-    );
-
-    std::optional<BookInfo> getBookDetails(const std::string& bookUrl);
-
-    bool downloadBook(const BookInfo& bookInfo, const fs::path& downloadDir,
-                      std::function<bool(cpr::cpr_off_t total, cpr::cpr_off_t downloaded)> progressCallback = nullptr);
 };
 
 #endif // ZLIBRARYAPI_HPP
