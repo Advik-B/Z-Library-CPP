54bd2caac0ab9274f6901b8e12ea331e4a0c88c7
Advik <advik.b@gmail.com>
2025-04-07 17:48:14 +0530

    Update ZLibraryAPI.cpp

--- src/ZLibraryAPI.cpp
+++ src/ZLibraryAPI.cpp
@@ -4,10 +4,17 @@
 
 #include <zlibrary/ZLibraryAPI.hpp>
 #include <zlibrary/NoCache.hpp>
+#include <zlibrary/Utils/UserAgent.hpp>
 #include <cpr/cpr.h>
 
 
 zlibrary::ZLibraryAPI::ZLibraryAPI() {
     this->base_url = "https://z-library.sk/";
     session.SetUrl(base_url);
+    session.SetHeader(
+            {
+                    {"User-Agent", zlibrary::utils::generate_useragent()}
+            }
+     );
+
 }
