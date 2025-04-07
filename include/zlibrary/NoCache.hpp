d11b75294e0cbf7dc5813cee6031e4a38b976681
Advik <advik.b@gmail.com>
2025-04-07 16:48:53 +0530

    Create NoCache.hpp

---
+++ include/zlibrary/NoCache.hpp
@@ -0,0 +1,25 @@
+//
+// Created by Advik on 07-04-2025.
+//
+
+#ifndef NOCACHE_HPP
+#define NOCACHE_HPP
+
+#include <zlibrary/ZCacheType.hpp>
+#include <zlibrary/Export.hpp>
+
+namespace zlibrary {
+
+    /*
+    Does not cache anything anywhere.
+     */
+    class NoCache : public ZCacheType {
+         void set(const string & key, const string& value) override;
+         string get(const string & key) override;
+         bool get(const string &key, string &result_string) override;
+         void purge() override;
+    };
+
+}
+
+#endif //NOCACHE_HPP
