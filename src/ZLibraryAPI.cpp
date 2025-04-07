2996d5fec71f0afd32b976293708b687a5985385
Advik <advik.b@gmail.com>
2025-04-07 18:31:00 +0530

    Update ZLibraryAPI.cpp

--- src/ZLibraryAPI.cpp
+++ src/ZLibraryAPI.cpp
@@ -2,19 +2,39 @@
 // Created by Advik on 07-04-2025.
 //
 
-#include <zlibrary/ZLibraryAPI.hpp>
+#include <cpr/cpr.h>
 #include <zlibrary/NoCache.hpp>
 #include <zlibrary/Utils/UserAgent.hpp>
-#include <cpr/cpr.h>
+#include <zlibrary/ZLibraryAPI.hpp>
+#include <memory>
 
+using namespace std;
 
+void zlibrary::ZLibraryAPI::setCache(CachePtr new_cache) {}
+void zlibrary::ZLibraryAPI::removeCache() {}
+void zlibrary::ZLibraryAPI::setBaseURL(const string &new_base_url) {}
+cpr::Response zlibrary::ZLibraryAPI::get(const string &path) {
+    return cpr::Get(path);
+}
+cpr::Response zlibrary::ZLibraryAPI::post(const string &path, const cpr::Payload &payload) {
+    return cpr::Post(path, payload);
+}
 zlibrary::ZLibraryAPI::ZLibraryAPI() {
     this->base_url = "https://z-library.sk/";
-    session.SetUrl(base_url);
-    session.SetHeader(
+    this->session = make_shared<cpr::Session>();
+    session->SetUrl(base_url);
+    session->SetHeader(
             {
-                    {"User-Agent", zlibrary::utils::generate_useragent()}
-            }
-     );
+                    {"User-Agent", utils::generate_useragent()},
+                    {"Accept-Language", "en-US,en;q=0.9"},
+                {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
+                {"Connection", "keep-alive"},
+                {"Upgrade-Insecure-Requests", "1"},
+                {"DNT", "1"},
+                {"Sec-Fetch-Dest", "document"}, // Common browser headers
+                 {"Sec-Fetch-Mode", "navigate"},
+                 {"Sec-Fetch-Site", "same-origin"}, // Adjust if needed, but good default
+                 {"Referer",  base_url }});
+    cache = std::make_shared<zlibrary::NoCache>();
 
 }
