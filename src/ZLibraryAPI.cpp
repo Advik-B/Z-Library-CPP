c127f7c685fb2f2e235fabdd08130106ef4dcb04
Advik <advik.b@gmail.com>
2025-04-07 17:33:00 +0530

    Update ZLibraryAPI.cpp

--- src/ZLibraryAPI.cpp
+++ src/ZLibraryAPI.cpp
@@ -3,5 +3,11 @@
 //
 
 #include <zlibrary/ZLibraryAPI.hpp>
+#include <zlibrary/NoCache.hpp>
+#include <cpr/cpr.h>
 
 
+zlibrary::ZLibraryAPI::ZLibraryAPI() {
+    this->base_url = "https://z-library.sk/";
+    session.SetUrl(base_url);
+}
