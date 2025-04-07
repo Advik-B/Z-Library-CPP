76ee811e0f5eeb1b99e23b61342bff4cd07351d8
Advik <advik.b@gmail.com>
2025-04-07 16:12:24 +0530

    Create BookStatus.hpp

---
+++ include/zlibrary/BookStatus.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Advik on 07-04-2025.
+//
+
+#ifndef BOOKSTATUS_HPP
+#define BOOKSTATUS_HPP
+
+#include <zlibrary/Export.hpp>
+namespace zlibrary {
+    enum class ZLIBRARY_API BookStatus {
+        AVAILABLE,
+        UNAVAILABLE,
+    };
+
+} // namespace zlibrary
+
+#endif //BOOKSTATUS_HPP
