03ede140d53633954655e67b3fdc582033bdf4f1
Advik <advik.b@gmail.com>
2025-04-07 16:15:46 +0530

    Create ZLibraryAPI.hpp

---
+++ include/zlibrary/ZLibraryAPI.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Advik on 07-04-2025.
+//
+
+#ifndef ZLIBRARYAPI_HPP
+#define ZLIBRARYAPI_HPP
+
+#include <zlibrary/Export.hpp>
+namespace zlibrary {
+    class ZLIBRARY_API ZLibraryAPI {
+        
+    };
+}
+
+#endif //ZLIBRARYAPI_HPP
