ba9f09696118dfaaf767f41355f0e4b2bb6611e7
Advik <advik.b@gmail.com>
2025-04-07 15:43:06 +0530

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -6,5 +6,5 @@ using namespace std;
 int main() {
     constexpr auto lang = zlibrary::Language::ARABIC;
     const string langStr = toString(lang);
-    cout << "Language enum to string: " << langStr << endl;
+    cout << "Lang: " + langStr << "\n";
 }
