e9f030908261644645cd965391519db3d1d38c99
Advik <advik.b@gmail.com>
2025-04-07 16:06:07 +0530

    Create Rating.hpp

---
+++ include/zlibrary/Rating.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Advik on 07-04-2025.
+//
+
+#ifndef RATING_HPP
+#define RATING_HPP
+
+#include <zlibrary/Export.hpp>
+
+namespace zlibrary {
+    struct ZLIBRARY_API Rating {
+        const unsigned int book_rating;
+        const unsigned int file_rating;
+    };
+}
+
+#endif //RATING_HPP
