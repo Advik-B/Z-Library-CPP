8b40a0a7b49edbc84d7786db34949ee6cca31339
Advik <advik.b@gmail.com>
2025-04-07 16:48:58 +0530

    Create ZCacheType.hpp

---
+++ include/zlibrary/ZCacheType.hpp
@@ -0,0 +1,26 @@
+//
+// Created by Advik on 07-04-2025.
+//
+
+#ifndef ZCACHETYPE_HPP
+#define ZCACHETYPE_HPP
+
+#include <string>
+#include <memory>
+#include <zlibrary/Export.hpp>
+
+namespace zlibrary {
+    using std::string;
+    using std::shared_ptr;
+    class ZLIBRARY_API ZCacheType {
+        public:
+            virtual void set(const string & key, const string& value) = 0;
+            virtual string get(const string & key) = 0;
+            virtual bool get(const string &key, string &result_string);
+            virtual void purge() = 0;
+    };
+
+    typedef shared_ptr<ZCacheType> ZCacheTypePtr;
+}
+
+#endif //ZCACHETYPE_HPP
