4dc09b5c039c80d652f9397f0b6c92b955f19eec
Advik <advik.b@gmail.com>
2025-04-07 17:48:08 +0530

    Update NoCache.cpp

--- src/NoCache.cpp
+++ src/NoCache.cpp
@@ -4,7 +4,7 @@
 
 #include <zlibrary/NoCache.hpp>
 
-inline void zlibrary::NoCache::set(const string & key, const string& value) {}
-inline std::string zlibrary::NoCache::get(const string & key) { return ""; }
-inline bool zlibrary::NoCache::get(const string &key, string &result_string) { return false; }
+inline void zlibrary::NoCache::set(const string &, const string&) {}
+inline std::string zlibrary::NoCache::get(const string &) { return ""; }
+inline bool zlibrary::NoCache::get(const string &, string &) { return false; }
 inline void zlibrary::NoCache::purge() {}
