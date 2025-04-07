7c793777a4c8a592c4e01840310b10328c0a9991
Advik <advik.b@gmail.com>
2025-04-07 16:49:02 +0530

    Update ZLibraryAPI.hpp

--- include/zlibrary/ZLibraryAPI.hpp
+++ include/zlibrary/ZLibraryAPI.hpp
@@ -6,9 +6,22 @@
 #define ZLIBRARYAPI_HPP
 
 #include <zlibrary/Export.hpp>
+#include <zlibrary/ZCacheType.hpp>
+#include <cpr/cpr.h>
+#include <string>
+
 namespace zlibrary {
+	using std::string;
     class ZLIBRARY_API ZLibraryAPI {
-        
+      	private:
+        	string base_url;
+            ZCacheTypePtr cache;
+
+		public:
+      		cpr::Session session;
+            void setBaseURL(const string& url);
+            string getBaseURL() const;
+
     };
 }
 
