7a43f1ecb658ef7970cc7cb6ac8e26bc194516c7
Advik <advik.b@gmail.com>
2025-04-07 16:12:27 +0530

    Update Book.hpp

--- include/zlibrary/Book.hpp
+++ include/zlibrary/Book.hpp
@@ -9,12 +9,15 @@
 #include <zlibrary/Category.hpp>
 #include <zlibrary/Enums.hpp>
 #include <zlibrary/Rating.hpp>
+#include <zlibrary/IPFS.hpp>
+#include <zlibrary/BookStatus.hpp>
 #include <string>
 
 namespace zlibrary {
     using std::string;
 
     struct ZLIBRARY_API Book {
+        const string url;
         const string title;
         const string author;
         const Rating rating;
@@ -22,9 +25,11 @@ namespace zlibrary {
         const string description;
         const Category category;
         const Language language;
+        const IPFS ipfs;
         const unsigned int filesize;
         const Extension extension;
         const string download_url;
+        const BookStatus status;
     };
 }
 
